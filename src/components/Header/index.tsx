c5f035e7272aac501bf1420442c85909daa2ef5f
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-04-18 16:51:38 -0300

    refactor: verify height and width in avatar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 ﻿import avatar from '../../assets/user_avatar.svg';
+import { faker } from '@faker-js/faker';
 
 export function Header(){
     return (
         <header className='h-20 bg-gray-400 flex space-x-4 items-center justify-end justify-items-end pr-5'>
             <span className='text-base font-normal'>Natália Lima de Oliveira</span>
-            <img src={avatar} alt="" />
+            <img className='rounded-full w-10 h-10' src={faker.image.avatar()} alt="" />
         </header>
     )
 }
